a21d0134d91bdfa092aa4d25cb46f2450b6783bd
dmwasielewski <wasielewski.damian@gmail.com>
2023-01-17 17:47:38 +0000

    small projects

--- squareDragAndDrop/app.js
+++ squareDragAndDrop/app.js
@@ -1,1 +1,36 @@
 'use strict';
+const div = document.querySelector('div');
+let divX = 150;
+let divY = 50;
+div.style.left = divX + 'px';
+div.style.top = `${divY}px`;
+
+let drawActive = false;
+
+let insertDivX;
+let insertDivY;
+
+div.addEventListener('mousedown', (e) => {
+  div.style.backgroundColor = 'red';
+  drawActive = !drawActive;
+
+  insertDivX = e.offsetX;
+  insertDivY = e.offsetY;
+  console.log(insertDivX, insertDivY);
+});
+
+div.addEventListener('mousemove', (e) => {
+  if (drawActive) {
+    divX = e.clientX - insertDivX;
+    divY = e.clientY - insertDivY;
+
+    div.style.left = `${divX}px`;
+    div.style.top = `${divY}px`;
+  }
+});
+
+div.addEventListener('mouseup', () => {
+  console.log('mouseUp');
+  div.style.backgroundColor = 'black';
+  drawActive = !drawActive;
+});
