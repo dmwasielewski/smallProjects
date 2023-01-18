f5c18f7a3ed7d08ec9c55ec8ab2de1033472cb10
dmwasielewski <wasielewski.damian@gmail.com>
2023-01-18 03:14:20 +0000

    added comments

--- squareDragAndDrop/app.js
+++ squareDragAndDrop/app.js
@@ -5,7 +5,7 @@ let divY = 50;
 div.style.left = divX + 'px';
 div.style.top = `${divY}px`;
 
-let drawActive = false;
+let drawActive = false; //flag to control the flow of a function or statement
 
 let insertDivX;
 let insertDivY;
@@ -14,7 +14,7 @@ div.addEventListener('mousedown', (e) => {
   div.style.backgroundColor = 'red';
   drawActive = !drawActive;
 
-  insertDivX = e.offsetX;
+  insertDivX = e.offsetX; // the outer border of the current element
   insertDivY = e.offsetY;
   console.log(insertDivX, insertDivY);
 });
