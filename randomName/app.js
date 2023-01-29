22453e66ff25c723ff122f929d380bc7349b823c
dmwasielewski <wasielewski.damian@gmail.com>
2023-01-29 13:43:20 +0000

    smallProjects

--- randomName/app.js
+++ randomName/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const btn = document.querySelector('.btn');
+const text = document.querySelector('.text');
+
+const names = [
+  'Adam',
+  'Arden',
+  'Azalea',
+  'Birdie',
+  'Blythe',
+  'Clover',
+  'Lilac',
+  'Lavender',
+  'Posey',
+  'Wally',
+];
+// console.log(names.length);
+const nameGenerator = () => {
+  const index = Math.floor(Math.random() * names.length);
+  text.textContent = `Best name for you is: ${names[index]}`;
+};
+
+btn.addEventListener('click', nameGenerator);
