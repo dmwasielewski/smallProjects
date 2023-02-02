b47d09e1d7c97c29a0f8d06006fbaa4207dce725
Damian Wasielewski <wasielewski.damian@gmail.com>
2023-02-02 01:58:50 +0000

    Update app.js

--- namesList/app.js
+++ namesList/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const div = document.querySelector('div');
+
+const names = [];
+
+const addName = (e) => {
+  e.preventDefault();
+  const input = document.querySelector('input');
+  const name = input.value;
+  names.push(name);
+  console.log(names);
+  div.textContent += name + ', ';
+  input.value = '';
+};
+
+const btn = document.querySelector('button').addEventListener('click', addName);
