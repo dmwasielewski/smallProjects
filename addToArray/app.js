b9ba470735c7356f25992a54a565a71bd625e124
dmwasielewski <wasielewski.damian@gmail.com>
2023-02-06 00:04:00 +0000

    smallProjects

--- addToArray/app.js
+++ addToArray/app.js
@@ -1,1 +1,24 @@
-'use strict';
+// 'use strict';
+const elementsArray = [];
+const div = document.querySelector('div');
+
+const addElement = (e) => {
+  e.preventDefault();
+
+  const input = document.querySelector('input');
+  const newElement = input.value;
+  if (input.value.length) {
+    for (element of elementsArray) {
+      if (element === newElement) {
+        alert('already exists');
+        return;
+      }
+    }
+    elementsArray.push(newElement);
+
+    div.textContent += newElement + ', ';
+    input.value = '';
+  }
+};
+
+document.querySelector('button').addEventListener('click', addElement);
