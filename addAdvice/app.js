856a724125b14dfb18472c4a6d9674f47355f87d
dmwasielewski <wasielewski.damian@gmail.com>
2023-02-06 01:10:09 +0000

    smallProjects

--- addAdvice/app.js
+++ addAdvice/app.js
@@ -1,1 +1,25 @@
-'use strict';
+// 'use strict';
+
+const adviceArray = [];
+const h1 = document.querySelector('h1');
+
+const addAdvice = (e) => {
+  e.preventDefault();
+
+  const input = document.querySelector('input');
+  const newAdvice = input.value;
+  if (input.value.length) {
+    for (element of adviceArray) {
+      if (element === newAdvice) {
+        alert('already exists');
+        return;
+      }
+    }
+    adviceArray.push(newAdvice);
+
+    h1.textContent += newAdvice + ', ';
+    input.value = '';
+  }
+};
+
+document.querySelector('.addAdvice').addEventListener('click', addAdvice);
