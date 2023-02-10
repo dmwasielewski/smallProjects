cfb1b16efb581d9349776fe25022e1ee8dc5eee3
dmwasielewski <wasielewski.damian@gmail.com>
2023-02-10 02:50:11 +0000

    smallProjects

--- textTypingEffect2/app.js
+++ textTypingEffect2/app.js
@@ -4,7 +4,27 @@ const spanCaret = document.querySelector('.caret');
 const spanText = document.querySelector('.text');
 const txt = ['test1', 'test2', 'test3'];
 
-const addLetter = () => {};
+let activeLetter = -15; //delayed start
+let activeText = 0;
+
+const addLetter = () => {
+  if (activeLetter >= 0) {
+    spanText.textContent += txt[activeText][activeLetter];
+  }
+  activeLetter++;
+  if (activeLetter === txt[activeText].length) {
+    activeText++; // next text in array
+    if (activeText === txt.length) return; // stop addLetter function
+
+    return setTimeout(() => {
+      activeLetter = -15; //reset letter index number from txt, with delayed start
+      spanText.textContent = '';
+      addLetter();
+    }, 2000);
+  }
+
+  setTimeout(addLetter, 100);
+};
 
 addLetter();
 
