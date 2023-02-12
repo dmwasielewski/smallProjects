be3adf9e0fa7dd745a07098002d7c0995fb4d017
dmwasielewski <wasielewski.damian@gmail.com>
2023-02-12 23:53:17 +0000

    smallProjects

--- closures3Timer/app.js
+++ closures3Timer/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const clock = () => {
+  let seconds = 0;
+  document.body.textContent = 0 + ' seconds';
+
+  const timer = () => {
+    seconds++;
+    document.body.textContent = seconds + ' seconds';
+  };
+  return timer;
+};
+
+const start = clock();
+
+setInterval(start, 1000);
