f3f0f02ef6a48138af59a76c7d7344d1ea20261d
dmwasielewski <wasielewski.damian@gmail.com>
2023-02-12 20:04:28 +0000

    smallProjects

--- closuresTimer/app.js
+++ closuresTimer/app.js
@@ -1,1 +1,14 @@
-'use strict';
+// 'use strict';
+
+const add = (start = 0) => {
+  let number = start;
+  return () => {
+    number++;
+    document.body.textContent = `Number =  ${number}`;
+  };
+};
+
+const counter = add();
+const counter5 = add(5);
+
+document.addEventListener('click', counter5);
