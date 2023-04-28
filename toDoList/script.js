629fd35aec773cb1cbddd442ed21c6c69b8ea068
dmwasielewski <wasielewski.damian@gmail.com>
2023-04-28 22:27:35 +0100

    testing quokka

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const itemForm = document.querySelector('#item-form');
-const itemInput = document.querySelector('#item-input');
+const itemInput = document.querySelector('#item-form-input');
 const itemList = document.querySelector('#item-list');
 
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+  //Validate input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+  console.log('success'); //?
+  console.log('ss'); //?
+}
+
 // Event listeners
+itemForm.addEventListener('submit', addItem);
