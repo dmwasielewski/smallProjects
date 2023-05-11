395ca8b4d75972b360302beed23a9332dbf18a78
dmwasielewski <wasielewski.damian@gmail.com>
2023-05-11 14:15:35 +0100

    Validate user input and handle the error

--- toDoList/script.js
+++ toDoList/script.js
@@ -3,24 +3,36 @@
 const itemForm = document.querySelector('#item-form');
 const itemInput = document.querySelector('#item-form-input');
 const itemList = document.querySelector('#item-list');
+// const buttonSub = document.querySelector('.btn-add');
 
-function addItem(e) {
-  e.preventDefault();
+function addItem(event) {
+  event.preventDefault();
 
-  const newItem = itemInput.value;
+  const newInputItem = itemInput.value;
 
-  // Validate input
-  if (newItem === '') {
-    alert('Please add an item');
-    return;
+  // Validate user input and handle the error
+  try {
+    if (newInputItem === '') {
+      throw new Error('Item missing');
+    }
+  } catch (error) {
+    console.error('User error:', error.message);
+    alert('Please write an item');
   }
 
   // Create list item
   const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
+  li.append(document.createTextNode(newInputItem));
 
-  const button = createButton('submit', addItem);
+  const button = createButton('remove-item btn-item-del');
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  return button;
 }
 
 // Event listeners
 itemForm.addEventListener('submit', addItem);
+// buttonSub.addEventListener('click', addItem);
