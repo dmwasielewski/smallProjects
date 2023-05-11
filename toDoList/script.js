738e588bdd5d3dac1a8a7d9a0c320fa84128fe73
dmwasielewski <wasielewski.damian@gmail.com>
2023-05-11 21:45:09 +0100

    Form & Filter min-width

--- toDoList/script.js
+++ toDoList/script.js
@@ -4,7 +4,7 @@ const itemForm = document.querySelector('#item-form');
 const itemInput = document.querySelector('#item-form-input');
 const itemList = document.querySelector('#item-list');
 const itemsClear = document.querySelector('#btn-clear');
-const itemFilter = document.querySelector('.item-filter');
+const itemFilter = document.querySelector('#item-filter');
 // const buttonSub = document.querySelector('.btn-add');
 
 function addItem(event) {
@@ -66,13 +66,42 @@ function clearItems() {
 function filterState() {
   if (itemList.innerHTML === '') {
     itemFilter.style.display = 'none';
+    itemList.style.display = 'none';
   } else {
     itemFilter.style.display = 'flex';
+    itemList.style.display = 'flex';
   }
 }
+
+// Filtering list
+function filterItems(event) {
+  const items = itemList.querySelectorAll('li'); //its NodeList
+  // const items = Array.from(itemList.querySelectorAll('li'));
+  // const items = [...itemList.querySelectorAll('li')];
+
+  const filterText = event.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.textContent.toLowerCase(); //its string not NodeList, so I can use .includes method
+    if (itemName.includes(filterText)) {
+      item.style.display = 'flex'; // Show matching items
+      // console.log(`Item text: ${itemName}`);
+      // console.log(Array.isArray(itemName));
+    } else {
+      item.style.display = 'none'; // Hide non-matching items
+    }
+  });
+
+  //   console.log(item);
+  // console.log(filterText);
+  // console.log(items);
+}
+
+// console.log(typeof itemList);
 
 // Event listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 itemsClear.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
 // buttonSub.addEventListener('click', addItem);
