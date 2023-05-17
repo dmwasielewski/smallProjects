06071000f74debea1527d1a190e6fb46a7bdc119
dmwasielewski <wasielewski.damian@gmail.com>
2023-05-17 14:56:40 +0100

    xhr states

--- JokeGenerator/app.js
+++ JokeGenerator/app.js
@@ -6,6 +6,12 @@ const generateJoke = () => {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
 
+  // ready Sate 5 possible values
+  // 0: request not initialized
+  // 1: server connection established
+  // 2: request received
+  // 3: processing request
+  // 4: request finished and response is ready
   xhr.onreadystatechange = function () {
     if (this.readyState === 4) {
       if (this.status === 200) {
